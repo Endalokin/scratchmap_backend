ce5aa1616cb3fe9070fe7af5b326692c43be5513
Nora Westphal <tupfen@hotmail.de>
2024-01-04 18:03:29 +0100

    removed unused routes, added index.html for a short documentation

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import contentfulRouter from "./src/router/contentful_router.js"
-import pgRouter from "./src/router/pg_router.js"
 import experienceRouter from "./src/router/experience_router.js"
 import tripRouter from "./src/router/trip_router.js"
+import fs from 'fs';
 
 const app = express()
 
@@ -13,12 +12,13 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
+const html = fs.readFileSync('index.html','utf8')
+app.get('/',(req,res)=>res.type('html').send(html));
+
 app.get("/test", (req, res) => {
     res.json({msg: `This is a test you got from the server. Hello ${process.env.SECRET_TEST}!`})
 })
 
-app.use("/contentful", contentfulRouter)
-app.use("/pg", pgRouter)
 app.use("/experiences", experienceRouter)
 app.use("/trips", tripRouter)
 
