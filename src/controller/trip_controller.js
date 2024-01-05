777f03c3fdcb107b10c6f554f75c33d97b9f6d52
Nora Westphal <tupfen@hotmail.de>
2024-01-05 15:06:59 +0100

    added route to calculate footprint without connection to db and roundtrip being optional

--- src/controller/trip_controller.js
+++ src/controller/trip_controller.js
@@ -5,32 +5,68 @@ import handleContent from "../utils/handleContent.js";
 
 const CARBONTRACER_KEY = process.env.CARBONTRACER_KEY;
 
+async function getFootprintFromCarbonTracer(query, res) {
+  const carbonTracerUrl = `https://api.carbontracer.uni-graz.at/routing/${CARBONTRACER_KEY}/${query.vehicle}/${query.departure}/${query.arrival}`;
+  let result;
+  await fetchData(carbonTracerUrl, (data) => {
+    console.log(data)
+    if (!data.response.success) {
+      res.json({msg: "Query failed!", errors: data.response.errors })
+      return 
+    }
+    result = {
+      distanceDirect: data.response.data.distanceDirect * (JSON.parse(query.roundtrip) ? 2 : 1),
+      distanceRoute: data.response.data.distanceRoute * (JSON.parse(query.roundtrip) ? 2 : 1),
+      emission:
+        query.vehicle == "car"
+          ? (data.response.data.co2eq * (JSON.parse(query.roundtrip) ? 2 : 1)) / query.travellers
+          : data.response.data.co2eq * (JSON.parse(query.roundtrip) ? 2 : 1),
+      amount:
+        query.vehicle == "car"
+          ? ((data.response.data.co2eq * (JSON.parse(query.roundtrip) ? 2 : 1)) / query.travellers / 1000) * 30
+          : ((data.response.data.co2eq * (JSON.parse(query.roundtrip) ? 2 : 1)) / 1000) * 30,
+      time: data.response.data.time,
+    };
+  });
+  res.json(result)
+}
+
 async function handleFootprint(id, body, res) {
   const carbonTracerUrl = `https://api.carbontracer.uni-graz.at/routing/${CARBONTRACER_KEY}/${body.vehicle}/${body.placeDeparture}/${body.placeArrival}`;
   let result;
-  await fetchData(carbonTracerUrl, (data) => {
-    result = {
-      distanceDirect: data.response.data.distanceDirect * 2,
-      distanceRoute: data.response.data.distanceRoute * 2,
-      emission: body.vehicle == "car" ? data.response.data.co2eq * 2 / body.travellers : data.response.data.co2eq * 2,
-      amount: body.vehicle == "car" ? (data.response.data.co2eq * 2 / body.travellers / 1000) * 30 : (data.response.data.co2eq * 2 / 1000) * 30,
-      time: data.response.data.time,
-    };
-    pool
-      .query(
-        "insert into footprint (travelid, distance, emission, amount, compensated, time) values ($1, $2, $3, $4, $5, $6)",
-        [
-          id,
-          result.distanceRoute,
-          result.emission,
-          result.amount,
-          false,
-          result.time,
-        ]
-      )
-      .then((data) => res.status(201).json(result))
-      .catch((err) => res.json({ msg: "transfer in db failed", err }));
-  }, "POST");
+  await fetchData(
+    carbonTracerUrl,
+    (data) => {
+      result = {
+        distanceDirect: data.response.data.distanceDirect * 2,
+        distanceRoute: data.response.data.distanceRoute * 2,
+        emission:
+          body.vehicle == "car"
+            ? (data.response.data.co2eq * 2) / body.travellers
+            : data.response.data.co2eq * 2,
+        amount:
+          body.vehicle == "car"
+            ? ((data.response.data.co2eq * 2) / body.travellers / 1000) * 30
+            : ((data.response.data.co2eq * 2) / 1000) * 30,
+        time: data.response.data.time,
+      };
+      pool
+        .query(
+          "insert into footprint (travelid, distance, emission, amount, compensated, time) values ($1, $2, $3, $4, $5, $6)",
+          [
+            id,
+            result.distanceRoute,
+            result.emission,
+            result.amount,
+            false,
+            result.time,
+          ]
+        )
+        .then((data) => res.status(201).json(result))
+        .catch((err) => res.json({ msg: "transfer in db failed", err }));
+    },
+    "POST"
+  );
 }
 
 export const tripController = {
@@ -38,6 +74,10 @@ export const tripController = {
     res.json(await handleContent("trip"));
   },
   updateFootprintTable: async (req, res) => {
+    // hier vermutlich besser erst Funktion await getFootprintFromCarbonTracer und dann Funktion zum Schreiben?
     handleFootprint(req.params.id, req.body, res);
+  },
+  calculateNext: async (req, res) => {
+    getFootprintFromCarbonTracer(req.query, res)
   },
 };
--- src/router/trip_router.js
+++ src/router/trip_router.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 router.get('/', tripController.getTrips)
 router.post('/:id', tripController.updateFootprintTable)
+router.get('/calculateNext', tripController.calculateNext)
 
 
 export default router;
